9cf0a8ef86ebaa90e1708a49415cc6dd343c23d2
dyljz12 <dyljz12@163.com>
2020-05-12 19:30:54 +0800

    Initial commit

---
+++ src/main/java/com/lz/itoa/ItoaApplication.java
@@ -0,0 +1,13 @@
+package com.lz.itoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lz/itoa/ItoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lz.itoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
