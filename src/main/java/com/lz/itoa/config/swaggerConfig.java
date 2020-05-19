b8506f367efaa9f689291ff1f7125fd0d253b594
dyljz12 <dyljz12@163.com>
2020-05-19 23:04:56 +0800

    增加swagger

---
+++ src/main/java/com/lz/itoa/config/swaggerConfig.java
@@ -0,0 +1,46 @@
+package com.lz.itoa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class swaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .pathMapping("/")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.lz.itoa.controller"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(new ApiInfoBuilder()
+                        .title("SpringBoot整合Swagger")
+                        .description("SpringBoot整合Swagger，详细信息......")
+                        .version("8.0")
+                        .contact(new Contact("啊啊啊啊","blog.csdn.net","aaa@gmail.com"))
+                        .license("The Apache License")
+                        .licenseUrl("http://www.baidu.com")
+                        .build());
+    }
+    //构建 api文档的详细信息函数,注意这里的注解引用的是哪个
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                //页面标题
+                .title("Spring Boot 使用 Swagger2 构建RESTful API")
+                //创建人
+                .contact(new Contact("Bryan", "http://blog.bianxh.top/", ""))
+                //版本号
+                .version("1.0")
+                //描述
+                .description("API 描述")
+                .build();
+    }
+}
--- src/main/java/com/lz/itoa/controller/HelloController.java
+++ src/main/java/com/lz/itoa/controller/HelloController.java
@@ -1,5 +1,11 @@
 package com.lz.itoa.controller;
 
+
+import com.lz.itoa.dao.UserMapper;
+import com.lz.itoa.entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "测试接口")
 @Slf4j
 @RestController
 public class HelloController {
     @Autowired
-    private TCourseMapper tCourseMapper;
+    private UserMapper userMapper;
+
+    @ApiOperation(value = "获取用户", notes="获取用户详细信息")
+    @ApiImplicitParam(name = "telephone", value = "电话号码", paramType = "query", required = true, dataType = "Integer")
     @GetMapping("/hello")
-    public List<TCourse> hello() {
-        List<TCourse> t_course = tCourseMapper.selectAll();
-        return t_course;
+    public List<User> hello() {
+        List<User> users = userMapper.selectAll();
+        return users;
     }
     @GetMapping("/admin")
     public String admin() {
